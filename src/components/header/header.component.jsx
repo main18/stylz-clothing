ffd0bc1fe18074d9cda1e52b2ba07b33146e3b98
Aymane Boumhaout <ayman.bmh123@gmail.com>
2020-05-13 01:25:35 +0000

    added the redirect after sign in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,10 +18,10 @@ const Header = ({ currentUser }) => {
             <div className='options'>
                 <Link className='option' to='/shop'>
                     SHOP
-                    </Link>
+                </Link>
                 <Link className='option' to=''>
                     CONTACT
-                    </Link>
+                </Link>
                 {currentUser ? (
                     <div className='option' onClick={() => auth.signOut()}>
                         SIGN OUT
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,15 +1,16 @@
+import { UserActionsTypes } from './user.types';
+
 const INITIALE_STATE = {
     currentUser: null
 }
 
 const userReducer = (state = INITIALE_STATE, action) => {
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionsTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload,
             }
-
         default:
             return state;
     }
